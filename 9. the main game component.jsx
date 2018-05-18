5ec07aa46de6416c72ac1cef67ce638ebe660198
wowdyaln <rogersmk@gmail.com.tw>
2018-05-18 19:54:37 +0800

    13 - Random Number of Stars

--- 9. the main game component.jsx
+++ 9. the main game component.jsx
@@ -1,9 +1,16 @@
 const Stars = (props)=> {
+  const numberOfStars = 1 + Math.floor(Math.random()*9)
+
+  let stars = []
+  for (let i = 0; i < numberOfStars; i++){
+    stars.push(
+      <i key={i} className="fa fa-star"></i>
+    )
+  }
+
   return(
     <div className="col-5">
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
+      {stars}
     </div>
   )
 }
